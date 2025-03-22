73d465a39cb2c04fde561282e6dd50c0349fd0f6
Ben Prew <ben@throwingbones.com>
2025-03-21 22:38:35 -0700

    move sprite loader to separate file

---
+++ game/sprites/loader.go
@@ -0,0 +1,53 @@
+package sprites
+
+import (
+	"bytes"
+	"image"
+	"image/color"
+
+	"github.com/hajimehoshi/ebiten/v2"
+)
+
+// LoadSpriteSheet loads the embedded SpriteSheet.
+func LoadSpriteSheet(sprWidth, sprHeight int, file []byte) ([][]*ebiten.Image, error) {
+	img, _, err := image.Decode(bytes.NewReader(file))
+	if err != nil {
+		return nil, err
+	}
+
+	bounds := img.Bounds()
+	width := bounds.Max.X - bounds.Min.X
+	height := bounds.Max.Y - bounds.Min.Y
+	rgba := image.NewRGBA(bounds)
+
+	tileWidth := width / sprWidth
+	tileHeight := height / sprHeight
+
+	// Convert indexed color to RGBA and set transparency
+	for y := bounds.Min.Y; y < bounds.Max.Y; y++ {
+		for x := bounds.Min.X; x < bounds.Max.X; x++ {
+			c := img.(*image.Paletted).ColorIndexAt(x, y)
+			if c == 255 { // Assuming index 255 is the transparent color
+				rgba.Set(x, y, color.RGBA{0, 0, 0, 0}) // Transparent
+			} else {
+				rgba.Set(x, y, img.At(x, y))
+			}
+		}
+	}
+	sheet := ebiten.NewImageFromImage(rgba)
+
+	// spriteAt returns a sprite at the provided coordinates.
+	spriteAt := func(x, y int) *ebiten.Image {
+		return sheet.SubImage(image.Rect(x*tileWidth, (y+1)*tileHeight, (x+1)*tileWidth, y*tileHeight)).(*ebiten.Image)
+	}
+
+	s := make([][]*ebiten.Image, sprHeight)
+	for y := 0; y < sprHeight; y++ {
+		s[y] = make([]*ebiten.Image, sprWidth)
+		for x := 0; x < sprWidth; x++ {
+			s[y][x] = spriteAt(x, y)
+		}
+	}
+
+	return s, nil
+}
--- game/sprites/player.go
+++ game/sprites/player.go
@@ -1,12 +1,8 @@
 package sprites
 
 import (
-	"bytes"
-	"image"
-	"image/color"
 	_ "image/png"
 
-	"github.com/benprew/s30/art"
 	"github.com/hajimehoshi/ebiten/v2"
 )
 
@@ -22,43 +18,10 @@ const (
 )
 
 type PlayerSprite struct {
-	Animations [8][5]*ebiten.Image
+	Animations [][]*ebiten.Image
 }
 
-// LoadSpriteSheet loads the embedded SpriteSheet.
-func LoadSpriteSheet(tileWidth, tileHeight int) (*PlayerSprite, error) {
-	img, _, err := image.Decode(bytes.NewReader(art.Ego_F_png))
-	if err != nil {
-		return nil, err
-	}
-
-	bounds := img.Bounds()
-	rgba := image.NewRGBA(bounds)
-
-	// Convert indexed color to RGBA and set transparency
-	for y := bounds.Min.Y; y < bounds.Max.Y; y++ {
-		for x := bounds.Min.X; x < bounds.Max.X; x++ {
-			c := img.(*image.Paletted).ColorIndexAt(x, y)
-			if c == 255 { // Assuming index 255 is the transparent color
-				rgba.Set(x, y, color.RGBA{0, 0, 0, 0}) // Transparent
-			} else {
-				rgba.Set(x, y, img.At(x, y))
-			}
-		}
-	}
-	sheet := ebiten.NewImageFromImage(rgba)
-
-	// spriteAt returns a sprite at the provided coordinates.
-	spriteAt := func(x, y int) *ebiten.Image {
-		return sheet.SubImage(image.Rect(x*tileWidth, (y+1)*tileHeight, (x+1)*tileWidth, y*tileHeight)).(*ebiten.Image)
-	}
-
-	s := &PlayerSprite{}
-	for y := range 8 {
-		for x := range 5 {
-			s.Animations[y][x] = spriteAt(x, y)
-		}
-	}
-
-	return s, nil
+func LoadPlayer(sprWidth, sprHeight int, file []byte) (*PlayerSprite, error) {
+	sheet, err := LoadSpriteSheet(sprWidth, sprHeight, file)
+	return &PlayerSprite{sheet}, err
 }
