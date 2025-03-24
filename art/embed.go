4678ae78dacd0f9866ddcbec8da5fb8dfeb1a472
Ben Prew <ben@throwingbones.com>
2025-03-23 19:21:26 -0700

    Update ebmed to use new sprite locations

--- art/embed.go
+++ art/embed.go
@@ -7,25 +7,35 @@ import (
 var (
 	// basic land tiles
 
-	//go:embed Landtile.spr.png
+	//go:embed sprites/world/land/Landtile.spr.png
 	Landtile_png []byte
 
 	// grass and shrubs
-	//go:embed Land.spr.png
+	//go:embed sprites/world/land/Land.spr.png
 	Land_png []byte
+	// grass and shrubs shadows
+	//go:embed sprites/world/land/Sland.spr.png
+	Sland_png []byte
 
 	// trees
-	//go:embed Land2.spr.png
+	//go:embed sprites/world/land/Land2.spr.png
 	Land2_png []byte
+	// trees
+	//go:embed sprites/world/land/Sland2.spr.png
+	Sland2_png []byte
 
 	// world frame
 	//go:embed Advinter1024.pic.png
 	WorldFrame_png []byte
 
 	// player characters
-	//go:embed Ego_M.spr.png
+	//go:embed sprites/world/characters/Ego_M.spr.png
 	Ego_M_png []byte
+	//go:embed sprites/world/characters/Sego_M.spr.png
+	Sego_M_png []byte
 
-	//go:embed Ego_F.spr.png
+	//go:embed sprites/world/characters/Ego_F.spr.png
 	Ego_F_png []byte
+	//go:embed sprites/world/characters/Sego_F.spr.png
+	Sego_F_png []byte
 )
