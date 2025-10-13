411ea63a68ca036a5add26c916a0700ee1c712b7
Ben Prew <ben@throwingbones.com>
2025-10-12 21:11:36 -0700

    Improve text multi-line rendering

--- game/ui/elements/text.go
+++ game/ui/elements/text.go
@@ -8,10 +8,12 @@ import (
 	"github.com/hajimehoshi/ebiten/v2/text/v2"
 )
 
+// TODO: TextBox should be a Rectangle and text should
+// flow into textbox size
 type Text struct {
 	text  string
 	font  text.Face
-	color color.Color
+	Color color.Color
 	X     int
 	Y     int
 }
@@ -23,16 +25,16 @@ func NewText(size float64, txt string, x, y int) *Text {
 		Size:   size,
 	}
 
-	return &Text{text: txt, font: fontFace, color: color.White, X: x, Y: y}
+	return &Text{text: txt, font: fontFace, Color: color.White, X: x, Y: y}
 }
 
 func (t *Text) Draw(screen *ebiten.Image, opts *ebiten.DrawImageOptions) {
-	R, G, B, A := t.color.RGBA()
-	options := &ebiten.DrawImageOptions{}
+	R, G, B, A := t.Color.RGBA()
+	options := &text.DrawOptions{}
 	options.GeoM.Concat(opts.GeoM)
 	options.GeoM.Translate(float64(t.X), float64(t.Y))
 	options.ColorScale.Scale(float32(R), float32(G), float32(B), float32(A))
-	textOpts := text.DrawOptions{DrawImageOptions: *options}
+	options.LineSpacing = 32.0
 
-	text.Draw(screen, t.text, t.font, &textOpts)
+	text.Draw(screen, t.text, t.font, options)
 }
