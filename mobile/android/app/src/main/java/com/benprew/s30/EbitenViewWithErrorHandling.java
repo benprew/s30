e6f67f8d956274a4ad6ad080b1d911ddb36c8048
Ben Prew <ben@throwingbones.com>
2026-03-25 11:29:48 -0700

    more android debugging logs, add internet permissions (card download)

--- mobile/android/app/src/main/java/com/benprew/s30/EbitenViewWithErrorHandling.java
+++ mobile/android/app/src/main/java/com/benprew/s30/EbitenViewWithErrorHandling.java
@@ -2,10 +2,13 @@ package com.benprew.s30;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.util.Log;
 
 import com.benprew.s30.mobile.EbitenView;
 
 class EbitenViewWithErrorHandling extends EbitenView {
+
+    private static final String TAG = "S30";
 
     public EbitenViewWithErrorHandling(Context context) {
         super(context);
@@ -17,6 +20,7 @@ class EbitenViewWithErrorHandling extends EbitenView {
 
     @Override
     protected void onErrorOnGameUpdate(Exception e) {
+        Log.e(TAG, "Game update error", e);
         super.onErrorOnGameUpdate(e);
     }
 }
--- mobile/android/app/src/main/java/com/benprew/s30/MainActivity.java
+++ mobile/android/app/src/main/java/com/benprew/s30/MainActivity.java
@@ -2,17 +2,23 @@ package com.benprew.s30;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import go.Seq;
 import com.benprew.s30.mobile.EbitenView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = "S30";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.i(TAG, "onCreate: setting content view");
         setContentView(R.layout.activity_main);
+        Log.i(TAG, "onCreate: setting Seq context");
         Seq.setContext(getApplicationContext());
+        Log.i(TAG, "onCreate: done");
     }
 
     private EbitenView getEbitenView() {
