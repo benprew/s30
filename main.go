04fff35e141d712b8bf428f328b2fb163c5dbbab
Ben Prew <ben@throwingbones.com>
2026-04-14 14:41:59 -0400

    add pprof server for live memory debugging

--- main.go
+++ main.go
@@ -6,18 +6,28 @@ import (
 	"strings"
 
 	"github.com/benprew/s30/game"
+	"github.com/benprew/s30/internal/pprofutil"
 	"github.com/benprew/s30/logging"
 	"github.com/hajimehoshi/ebiten/v2"
 )
 
 func main() {
 	verbose := flag.String("v", "", "enable verbose logging for subsystems (comma-separated: mtg,world,duel)")
+	pprofAddr := flag.String("pprof", "", "enable pprof HTTP server at the given listen address, e.g. 127.0.0.1:6060")
 	flag.Parse()
 
 	if *verbose != "" {
 		for _, s := range strings.Split(*verbose, ",") {
 			logging.Enable(logging.Subsystem(strings.TrimSpace(s)))
 		}
+	}
+
+	pprofLn, err := pprofutil.Start(*pprofAddr, log.Printf)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if pprofLn != nil {
+		defer pprofLn.Close()
 	}
 
 	ebiten.SetWindowTitle("Shandalar 30")
