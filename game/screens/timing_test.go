4220e4f3110db1a32c33af498b00356292d1b267
Ben Prew <ben@throwingbones.com>
2026-08-19 12:38:11 -0700

    Remove cityAnimation, that's part of another git branch

--- game/screens/timing_test.go
+++ game/screens/timing_test.go
@@ -6,7 +6,4 @@ func TestTickBasedScreenTimingPreservesTenTPSDurations(t *testing.T) {
 	if dungeonAmbientTicks != 1800 {
 		t.Errorf("dungeonAmbientTicks = %d, want 1800", dungeonAmbientTicks)
 	}
-	if cityEntranceAnimationFrames != 120 {
-		t.Errorf("cityEntranceAnimationFrames = %d, want 120", cityEntranceAnimationFrames)
-	}
 }
